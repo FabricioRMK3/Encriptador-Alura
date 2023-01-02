e948e216fc12185c83e5fe10ed8c080e55bd7335
FabricioRMK3 <fabriciovc16@gmail.com>
2023-01-02 18:44:03 -0500

    pequeño cambio

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 
+
+// FUNCION PARA CREAR EL DIV DONDE IRÁ EL RESULTADO DE LA ENCRIPTACION Y EL BOTON DE COPIAR
+function crearRespuesta(textoResultado){
+  let resultado = document.getElementById("resultado");
+  resultado.style.display = "block";
+  resultado.innerHTML = "";
+  let parrafo  = document.createElement("p");
+  parrafo.id = "texto-salida";
+  parrafo.innerHTML = textoResultado;
+  resultado.appendChild(parrafo);
+  let botonCopiar = document.createElement("button");
+  botonCopiar.id = "btn-copiar";
+  botonCopiar.className = "botones"
+  botonCopiar.innerHTML = "Copiar";
+  botonCopiar.addEventListener("click", copiarTexto);
+  resultado.appendChild(botonCopiar);
+  
+  document.getElementById("invisible").style.display = "none";
+}
+
+
+
+
+
+//FUNCION PARA ENCRIPTAR EL TEXTO
 function encriptar() {
     let texto = document.getElementById("texto-entrada").value;
     let textoEncriptado = "";
@@ -18,26 +43,72 @@ function encriptar() {
         textoEncriptado += c;
       }
     }
-    let resultado = document.getElementById("resultado");
-    resultado.style.display = "block";
-    resultado.innerHTML = "";
-    let parrafo  = document.createElement("p");
-    parrafo.id = "texto-salida";
-    parrafo.innerHTML = textoEncriptado;
-    resultado.appendChild(parrafo);
-    let botonCopiar = document.createElement("button");
-    botonCopiar.id = "btn-copiar";
-    botonCopiar.className = "botones"
-    botonCopiar.innerHTML = "Copiar";
-    botonCopiar.addEventListener("click", copiarTexto);
-    resultado.appendChild(botonCopiar);
+    crearRespuesta(textoEncriptado)
     
     document.getElementById("invisible").style.display = "none";
 
   }
 
-  function copiarTexto() {
-    let texto = document.getElementById("texto-salida");
-    texto.select();
-    document.execCommand("copy");
+// FUNCION PARA DESENCRIPTAR EL TEXTO
+
+function desencriptar() {
+  
+  let texto = document.getElementById("texto-entrada").value;
+  let textoDesencriptado = desEncriptar(texto);
+
+  crearRespuesta(textoDesencriptado)
+}
+
+function desEncriptar(texto){
+  let palabra = '';
+  if(texto !== ''){
+    palabra += texto.replaceAll('ai','a').replaceAll('enter','e').replaceAll('imes','i').replaceAll('ober','o').replaceAll('ufat','u')
   }
+  return palabra
+}
+
+// CONVIERTE CUALQUIER TEXTO CON MAYUSCULAS O CON TILDES EN TEXTO PLANO
+window.addEventListener("load", function() {
+  let elemento = document.getElementById("texto-entrada");
+
+  elemento.addEventListener("input", function() {
+    this.value = this.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  });
+});
+
+
+// FUNCION PARA COPUAR EL TEXTO DESENCRIPTADO
+function copiarTexto() {
+  let texto = document.getElementById("texto-salida");
+  let seleccion = window.getSelection();
+  let rango = document.createRange();
+  rango.selectNodeContents(texto);
+  seleccion.removeAllRanges();
+  seleccion.addRange(rango);
+  document.execCommand("copy");
+}
+
+
+
+
+
+
+
+
+// const codigo = {
+//   e: "enter",
+//   i: "imes",
+//   a: "ai",
+//   o: "ober",
+//   u: "ufat",
+// };
+
+// function encriptar(texto) {
+//   return texto.replace(/[aeiou]/g, (c) => codigo[c]);
+// }
+
+// function desencriptar(texto) {
+//   return texto.replace(/(enter|imes|ai|ober|ufat)/g, (c) => Object.keys(codigo).find((k) => codigo[k] === c));
+// }
+
+
